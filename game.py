52375c990a722e80b7df954e70e310fbf35dd9a8
averysomers <84360283+averysomers@users.noreply.github.com>
2021-05-25 21:59:55 -0400

    Simplified nested ifs and added text for winning/losing

--- game.py
+++ game.py
@@ -1,8 +1,11 @@
 
 # game.py
 
+print("Ready, Player One?")
+
 print("Rock, Paper, Scissors, Shoot!")
 
+from hashlib import scrypt
 import random
 
 #print(10)
@@ -34,37 +37,72 @@ else:
     print("OOPS, invalid input. Please try again.")
     exit()
 
-
-valid_options = ["rock", "paper", "scissors"]
+ROCK = "rock"
+PAPER = "paper"
+SCISSORS = "scissors"
+valid_options = [ROCK, PAPER, SCISSORS]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE: ", computer_choice)
 
-if (user_choice == "rock") and (computer_choice == "scissors"):
-    print("Rock beats Scissors")
-else:
-    if(user_choice == "rock") and (computer_choice == "paper"):
-        print("Paper beats Rock")
-    else:
-        if(user_choice == "rock") and (computer_choice == "rock"):
-            print("Tie")
-        else:
-            if(user_choice == "scissors") and (computer_choice == "scissors"):
-                print("Tie")
-            else:
-                if(user_choice == "scissors") and (computer_choice == "rock"):
-                    print("Rock beats Scissors")
-                else:
-                    if(user_choice == "scissors") and (computer_choice == "paper"):
-                        print("Scissors beats Paper")
-                    else:
-                        if(user_choice == "paper") and (computer_choice == "rock"):
-                            print("Paper beats Rock")
-                        else:
-                            if(user_choice == "paper") and (computer_choice == "scissors"):
-                                print("Scissors beats Paper")
-                            else:
-                                if(user_choice == "paper") and (computer_choice == "paper"):
-                                    print("Tie")
-                                    exit()
 
+if (user_choice == ROCK):
+    if (computer_choice == SCISSORS):
+        print(ROCK+" beats "+SCISSORS)
+        print("Congrats, Player One! You win.")
+    if (computer_choice == ROCK):
+        print("Tie")
+    if (computer_choice == PAPER):
+        print(PAPER +" beats " +ROCK)
+        print("Sorry, Player One. You lose.")
+
+if (user_choice == SCISSORS):
+    if (computer_choice == SCISSORS):
+        print("Tie")
+    if (computer_choice == ROCK):
+        print(ROCK +" beats " +SCISSORS)
+        print("Sorry, Player One. You lose.")
+    if (computer_choice == PAPER):
+        print(SCISSORS +" beats " +PAPER)
+        print("Congrats, Player One! You win.")
+
+if (user_choice == PAPER):
+    if (computer_choice == SCISSORS):
+        print(SCISSORS+" beats "+PAPER)
+        print("Sorry, Player One. You lose.")
+    if (computer_choice == ROCK):
+        print(PAPER +" beats " +ROCK)
+        print("Congrats, Player One! You win.")
+    if (computer_choice == PAPER):
+        print("Tie")
+
+
+
+#if (user_choice == "rock") and (computer_choice == "scissors"):
+#    print("Rock beats Scissors")
+#else:
+#    if(user_choice == "rock") and (computer_choice == "paper"):
+#        print("Paper beats Rock")
+#    else:
+#        if(user_choice == "rock") and (computer_choice == "rock"):
+#            print("Tie")
+#        else:
+#            if(user_choice == "scissors") and (computer_choice == "scissors"):
+#                print("Tie")
+#            else:
+#                if(user_choice == "scissors") and (computer_choice == "rock"):
+#                    print("Rock beats Scissors")
+#                else:
+#                    if(user_choice == "scissors") and (computer_choice == "paper"):
+#                        print("Scissors beats Paper")
+#                    else:
+#                        if(user_choice == "paper") and (computer_choice == "rock"):
+#                            print("Paper beats Rock")
+#                        else:
+#                            if(user_choice == "paper") and (computer_choice == "scissors"):
+#                                print("Scissors beats Paper")
+#                            else:
+#                                if(user_choice == "paper") and (computer_choice == "paper"):
+#                                    print("Tie")
+#                                    exit()
+#
 print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
